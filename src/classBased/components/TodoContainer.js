6acbb9f2470c9fbff491d46a6f68915523a83c20
Jonathas Tavares <jonathastavares@id.uff.br>
2021-11-01 21:28:37 -0300

    Refactor code with react hooks

--- src/classBased/components/TodoContainer.js
+++ src/classBased/components/TodoContainer.js
@@ -3,26 +3,29 @@ import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
+
 class TodoContainer extends React.Component {
     state = {
-        todos: [
-          {
-            id: uuidv4(),
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: uuidv4(),
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: uuidv4(),
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
+        todos: []
        };
+
+       componentDidMount() {
+        let temp = localStorage.getItem("todos")
+        let loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
+      }
+    
+      componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }            
+      }
+
        handleChange = id => {
         this.setState(prevState => ({
             todos: prevState.todos.map(todo => {
