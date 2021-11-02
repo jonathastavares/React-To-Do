7a8ab31b64bb450df42de9fc848f1c0cf63161ae
Jonathas Tavares <jonathastavares@id.uff.br>
2021-11-01 22:09:23 -0300

    Finish page layout design

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -3,11 +3,10 @@ import { Link, useRouteMatch, Route } from "react-router-dom";
 import SinglePage from "./SinglePage";
 
 const About = () => {
-    const { url, path } = useRouteMatch();
-  
+    const { url, path } = useRouteMatch()
     return (
-        <div>
-          <ul>
+      <div className="about__content">
+        <ul className="about__list">
             <li>
               <Link to={`${url}/about-app`}>About App</Link>
             </li>
